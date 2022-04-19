5b96c731d6bd9889666b92bd43247faa1c4308ad
Danilo Ferreira <dan.ali.ferreira@hotmail.com>
2022-04-19 19:07:45 -0300

    1904

--- logica_combustivel.js
+++ logica_combustivel.js
@@ -13,11 +13,11 @@ function calcular(){
     opcao = opcoes.value
     if (opcao == "gasolina"){
         vc = 7.267
-        kmlm = 11
+        kmlm = 11.98
     }
     if (opcao == "etanol"){
         vc = 4,646
-        kmlm = 8
+        kmlm = 8.25
     }
     if(document.getElementById('Valorporlitro')){
         var v_l = document.getElementById('Valorporlitro')
@@ -55,160 +55,126 @@ function calcular(){
         erroqtdd++
     }
     teste_n_parametros()
-    if (vl && ql && !vtot && !qui && !control){
-        vtot = vl * ql
-        if (kml){
-            qui = ql * kml
-        }else{
-            kml = kmlm
-            qui = ql * kml
-        }
-        quil.value = qui
-        km_l.value = kml
-        v_tot.value = vtot.toFixed(2)
-        control = true
-    }
-    if (vl && vtot && !ql && !qui  && !control){
-        ql = vtot / vl
-        if (kml){
-            qui = ql * kml
-        }else{
-            kml = kmlm
-            qui = ql * kml
-        }
-        quil.value = qui
-        km_l.value = kml
-        q_l.value = ql
-        control = true
-    }
-    if (vtot && ql && !vl && !qui  && !control){
+    
+    if (qui && vtot && ql && !vl && !kml){
         vl = vtot / ql
-        if (kml){
-            qui = ql * kml
+        kml = qui /ql
+        exibir()
+        control = true
+    }
+    if (qui && vtot && !ql){
+        if(vl){
+            ql = vtot / vl
+            kml = qui / ql
+        }else{
+            if(kml){
+                ql = qui / kml
+                vl = vtot/ql
+            }else{
+                vl = vc
+                ql = vtot / vl
+                kml = qui / ql
+            }
+        }
+        exibir()
+        control = true
+    }
+    if(qui && ql && !vtot){
+        kml = qui / ql
+        if(vl){
+            vtot = vl * ql
+        }else{
+            vl = vc
+            vtot = vl * ql
+        }
+        exibir()
+        control = true
+    }
+    if (vtot && ql && !qui){
+        vl = vtot / ql
+        if(kml){
+            qui = kml * ql
         }else{
             kml = kmlm
-            qui = ql * kml
+            qui = kml * ql
         }
-        quil.value = qui
-        km_l.value = kml
-        v_l.value = vl
-        control = true
+        exibir()
+        control =  true
     }
-    /// ql qui
-    if (ql && qui && !vtot  && !control){
-        if (vl){
-            vl = vl   
-        }else{
-            vl = vc
-        }
-        if(kml){
-            vtot = vl * ql
-        }else{
-            kml = qui / ql
-            vtot = vl * ql
-        }
-        km_l.value = kml
-        v_tot.value = vtot.toFixed(2)
-        control = true
-    }
-    if (ql && qui && !vl  && !control){
-        if (vtot){
-            vtot = vtot
-        }else{
-            vtot = vc * ql
-        }
-        if(kml){
-            kml = kml
-        }else{
-            kml = qui / ql
-        }
-        v_l.value = vl
-        km_l.value = kml
-        v_tot.value = vtot.toFixed(2)
-        control = true
-    }
-    if (qui && kml && !ql  && !control){
-        ql = qui / kml
-        if (vl && !vtot){
-            vtot = vl *ql
-        }
-        if (!vl && vtot){
-           vl = vtot / ql
-        }
-        if (!vl && !vtot){
-            vl =vc
-            vtot = vl * ql
-        }
-        v_l.value = vl
-        v_tot.value = vtot.toFixed(2)
-        q_l.value = ql
-        control = true
-
-    }
-    if (ql && kml && !qui  && !control){
-        qui = ql * kml
-        if (vl && !vtot){
-            vtot = vl *ql
-        }
-        else if (!vl && vtot){
-           vl = vtot / ql
-        }
-        else if (!vl && !vtot){
-            vl = vc
-            vtot = vl * ql
-        }
-        v_l.value = vl
-        v_tot.value = vtot.toFixed(2)
-        quil.value = qui
-        control = true
-    }
-    if (kml && vtot && !qui && !ql){
+    if (vtot && !qui && !ql){
         if (vl){
             ql = vtot / vl
         }else{
             vl = vc
             ql = vtot / vl
         }
-        qui = ql * kml
-        v_l.value = vl
-        q_l.value = ql.toFixed(2)
-        quil.value = qui
-        control = true
-    }
-    if (qui && vl && !ql) {
-        if (kml){
-            ql = qui / kml
+        if(kml){
+            qui = ql * kml
         }else{
             kml = kmlm
-            ql = qui /kml
+            qui = ql * kml
         }
-        vtot = ql * vl
-        v_tot.value.vtot.toFixed(2)
-        km_l.value = kml
-        q_l.value = ql.toFixed(2)
-        control = true
-    } 
-    if (qui && vtot && !!ql){
-        if (kml){
-            ql = qui / kml
-        }else{
-            kml = kmlm
-            ql = qui / kml
-        }
-        if (vl){
-            vl = vl
-        }else{
-            vl = vtot / ql
-        }
-        vtot = ql * vl
-        km_l.value = kml
-        q_l.value = ql.toFixed(3)
-        v_l.value = vl.toFixed(3)
+        exibir()
         control = true
     }
-    if (kml && vl && !vtot && !ql && !qui){
+    if (qui && !vtot && !ql){
+        if(kml){
+            ql = qui / ql
+        }else{
+            kml = kmlm
+            ql = qui / ql
+        }
+        if(vl){
+            vtot = ql * vl
+        }else{
+            vl = vc
+            vtot = ql * vl
+        }
+        exibir()
+        control = true
+    }
+    if (ql && !qui && !vtot){
+        if(vl){
+            vtot = ql * vl
+        }else{
+            vl = vc
+            vtot = ql * vl
+        }
+        if (kml){
+            qui = ql * kml
+        }else{
+            kml = kmlm
+            qui = ql * kml
+        }
+        exibir()
+        control = true
+    }
+    if(((vl && kml)||(!vl && kml)||(vl && !kml)) && !ql && !qui && !vtot){
         erroparametro()
-        control = true
+        control = true 
+    }
+
+    function exibir(){
+        if(!v_l){
+            v_l = document.getElementById('Valorporlitro')
+        }
+        if(!q_l){
+            q_l = document.getElementById('Qtdddelitros')
+        }
+        if(!quil){
+            quil = document.getElementById('Quilomêtros')
+        }
+        if(!km_l){
+            km_l = document.getElementById('Kmporlitro')
+        }
+        if(!v_tot){
+            v_tot = document.getElementById('Valortotal')
+        }
+        quil.value = qui
+        km_l.value = kml.toFixed(2)
+        q_l.value = ql.toFixed(2)
+        v_l.value = vl.toFixed(3)
+        v_tot.value = vtot.toFixed(2)
     }
 }
 function limpar(){
@@ -217,19 +183,21 @@ function limpar(){
 function erroparametro(){
     var txt = document.createElement('label')
     txt.classList = "erro"
-    txt.innerHTML = "Não é possível realizar operação com esses dois parâmetros. "
+    txt.innerHTML = "Não é possível realizar operação com esse parâmetro "
     campos.appendChild(txt)
 }
 function teste_n_parametros(){
     var txt = document.createElement('label')
     txt.classList = "erro"
-    if(erroqtdd>=4){
-        txt.innerHTML = "Não é possível realizar operação insira no maximo 3 parâmetros. "
+    if(erroqtdd>4){
+        txt.innerHTML = "Não é possível realizar  esta operação, insira no maximo 4 parâmetros. "
         campos.appendChild(txt)
+        control = true
     }
-    if(erroqtdd<=1){
-        txt.innerHTML = "Não é possível realizar esta operação, insira no minímo 2 parâmetros. "
+    if(erroqtdd = 0){
+        txt.innerHTML = "Não é possível realizar esta operação, insira no minímo 1 parâmetros. "
         campos.appendChild(txt)
+        control = true
     }
     
 }
