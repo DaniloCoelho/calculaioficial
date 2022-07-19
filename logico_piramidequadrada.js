ba3c440ce3c5d6ba956c40a6772c5c0503d0688f
Danilo Ferreira <dan.ali.ferreira@hotmail.com>
2022-07-19 00:14:16 -0300

    1907

--- logico_piramidequadrada.js
+++ logico_piramidequadrada.js
@@ -1,103 +1,340 @@
 var campos = document.getElementById('campos')
-var pi = 3.14159265359
-var a = 0
-var b = 0
-var c = 0
-var delta =0
+var x =0
 
 function calcular(){
     if(document.getElementById('altura')){
         var altura = document.getElementById('altura')
         var alt = Number(altura.value)
     }
-    if(document.getElementById('raio')){
-        var raio = document.getElementById('raio')
-        var rai = Number(raio.value)
+    if(document.getElementById('ladobase')){
+        var ladobase = document.getElementById('ladobase')
+        var lb = Number(ladobase.value)
     }
-    if(document.getElementById('diametro')){
-        var diametro = document.getElementById('diametro')
-        var dia = Number(diametro.value)
+    if(document.getElementById('geratriz')){
+        var geratriz = document.getElementById('geratriz')
+        var gtriz = Number(geratriz.value)
     }
-    if(document.getElementById('perimetro')){
-        var perimetro = document.getElementById('perimetro')
-        var per = Number(perimetro.value)
+    if(document.getElementById('aresta')){
+        var aresta = document.getElementById('aresta')
+        var arestapi = Number(aresta.value)
     }
     if(document.getElementById('area')){
         var area = document.getElementById('area')
         var are = Number(area.value)
     }
-    if(document.getElementById('volume')){
-        var volume = document.getElementById('volume')
-        var vol = Number(volume.value)
+    if(document.getElementById('apótemabase')){
+        var apotemabase = document.getElementById('apótemabase')
+        var apobase = Number(apotemabase.value)
     }
-    if(document.getElementById('geratriz')){
-        var geratriz = document.getElementById('geratriz')
-        var ger = Number(geratriz.value)
+    if(document.getElementById('flateral')){
+        var flateral = document.getElementById('flateral')
+        var fl = Number(flateral.value)
     }
     var control = false
+
+        // r = lb / 2
+        // g² =(alt)² + (lb/2)²
+        // ap² = (g)² + r²
+        // fl = g * r
+        // ab = lb²
+
+    if(alt && lb && !control){
+        apobase = lb / 2
+        gtriz = ((alt)**2 + (apobase)**2 )**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        are = lb**2
+        control = true   
+    }
+    if(alt && apobase && !control){
+        lb = apobase *2
+        gtriz = ((alt)**2 + (apobase)**2 )**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        are = lb**2
+        control = true
+    }
+    if(alt && gtriz && !control){
+        if(alt> gtriz){
+            //erro
+        }
+        apobase = ((gtriz)**2 - (alt)**2)**(1/2)
+        lb = apobase *2
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        are = lb**2
+        control = true
+    }
+    if(alt && arestapi && !control){
+        if(alt> arestapi){
+            //erro
+        }
+        // sen de 45° 0,7071067812      cateto oposto = sen45° * hipotenusa
+        x = ((arestapi**2) - (alt**2) )**(1/2)
+        apobase = 0.70710678118654752440084436210485 * x
+        lb = apobase *2
+        gtriz = ((alt)**2 + (apobase)**2 )**(1/2)
+        fl = gtriz * apobase
+        are = lb**2
+        control = true
+
+    }
+    if(alt && are && !control){
+        lb = are**(1/2)
+        apobase = lb / 2
+        gtriz = ((alt)**2 + (apobase)**2 )**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        control = true
+
+    }
+    if(alt && fl && !control){
+        if(alt > fl){
+            //erro
+        }
+        apobase = alt/2 
+        gtriz = ((alt)**2 + (apobase)**2 )**(1/2)
+        x = gtriz * apobase
+        while((fl < x-0.001) || (fl > x+0.001)){
+            gtriz = ((alt)**2 + (apobase)**2 )**(1/2)
+            x = gtriz * apobase
+            if(fl > x){
+                apobase += 0.0001
+                if(fl < x){
+                    apobase -= 0.0001
+                    while(fl > x){
+                        apobase += 0.0000001
+                    }
+                    break
+                }
+            }
+            if(fl < x){
+                apobase -= 0.0001
+                if(fl > x){
+                    apobase += 0.0001
+                    while(fl < x){
+                        apobase -= 0.0000001
+                    }
+                    break
+
+                }
+            }
+        }
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        lb = apobase *2
+        are = lb**2
+        control = true
+        //gtriz = ((alt)**2 + (apobase)**2 )**(1/2)
+        //x = fl / gtriz
+
+    }
+    if(lb && gtriz && !control){
+        apobase = lb / 2
+        if(apobase> gtriz){
+            //erro
+        }
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        are = lb**2
+        control = true
+    }
+    if(lb && arestapi && !control){
+        apobase = lb / 2
+        gtriz =((arestapi)**2 - (apobase)**2)**(1/2)
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        are = lb**2
+        control = true
+
+    }
+    if(lb && are && !control){
+        //ver
+    }
+    if(lb && apobase && !control){
+        //ver
+    }
+    if(lb && fl && !control){
+        apobase = lb / 2
+        gtriz = fl / apobase
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        are = lb**2
+        control = true
+    }
+    if(gtriz && arestapi && !control){
+        if(gtriz> arestapi){
+            //erro
+        }
+        apobase =((arestapi)**2 - (gtriz)**2)**(1/2)
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        lb = apobase *2
+        are = lb**2
+        fl = gtriz * apobase
+        control = true
+    }
+    if(gtriz && are && !control){
+        lb = are**(1/2)
+        apobase = lb /2
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        control = true
+    }
+    if(gtriz && apobase && !control){
+        if(gtriz< apobase){
+            //erro
+        }
+        lb = apobase *2
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        are = lb**2
+        control = true
+    }
+    if(gtriz && fl && !control){
+        if(gtriz> fl){
+            //erro
+        }
+        apobase = fl /gtriz
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        lb = apobase *2
+        are = lb**2
+        control = true
+    }
+    if(arestapi && are && !control){
+        lb = are**(1/2)
+        apobase = lb /2
+        gtriz =((arestapi)**2 - (apobase)**2)**(1/2)
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        control = true
+
+    }
+    if(arestapi && apobase && !control){
+        if(apobase> arestapi){
+            //erro
+        }
+        lb = apobase *2
+        gtriz =((arestapi)**2 - (apobase)**2)**(1/2)
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        fl = gtriz * apobase
+        are = lb**2
+        control = true
+    }
+    if(arestapi && fl && !control){
+        if(fl < arestapi){
+            //erro
+        }
+        apobase = alt/2 
+        gtriz =((arestapi)**2 - (apobase)**2)**(1/2)
+        x = gtriz * apobase
+        while((fl < x-0.001) || (fl > x+0.001)){
+            gtriz =((arestapi)**2 - (apobase)**2)**(1/2)
+            x = gtriz * apobase
+            if(fl > x){
+                apobase += 0.0001
+                if(fl < x){
+                    apobase -= 0.0001
+                    while(fl > x){
+                        apobase += 0.0000001
+                    }
+                    break
+                }
+            }
+            if(fl < x){
+                apobase -= 0.0001
+                if(fl > x){
+                    apobase += 0.0001
+                    while(fl < x){
+                        apobase -= 0.0000001
+                    }
+                    break
+
+                }
+            }
+        }
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        lb = apobase *2
+        are = lb**2
+        control = true
+        
+    }
+    if(are && apobase){
+        //nao da
+    }
+    if(are && fl && !control){
+        lb = are**(1/2)
+        apobase = lb /2
+        gtriz = fl / apobase
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        control = true
+    }
+    if(apobase && fl && !control){
+        if(apobase > fl){
+            //erro
+        }
+        gtriz = fl / apobase
+        alt = ((gtriz)**2 - (apobase)**2)**(1/2)
+        arestapi =((gtriz)**2 + (apobase)**2)**(1/2)
+        lb = apobase *2
+        are = lb**2
+        control = true
+    }
+    exibir()
 
     
 
     function exibir(){
         if(altura){
-            altura.value = alt.toFixed(2)
+            altura.value = alt.toFixed(3)
         }else{
             altura = document.getElementById('altura')
-            altura.value = alt.toFixed(2)
+            altura.value = alt.toFixed(3)
         }
-        if(raio){
-            raio.value = rai.toFixed(2)
+        if(ladobase){
+            ladobase.value = lb.toFixed(3)
         }
         else{
-            raio = document.getElementById('raio')
-            raio.value = rai.toFixed(2)
+            ladobase = document.getElementById('ladobase')
+            ladobase.value = lb.toFixed(3)
         }
-        if(diametro){
-            diametro.value = dia.toFixed(2)
+        if(geratriz){
+            geratriz.value = gtriz.toFixed(3)
     
         }else{
-            diametro = document.getElementById('diametro')
-            diametro.value = dia.toFixed(2)
+            geratriz = document.getElementById('geratriz')
+            geratriz.value = gtriz.toFixed(3)
             
         }
-        if(perimetro){
-            perimetro.value = per.toFixed(2)
+        if(aresta){
+            aresta.value = arestapi.toFixed(3)
         }else{
-            perimetro = document.getElementById('perimetro')
-            perimetro.value = per.toFixed(2)
+            aresta = document.getElementById('aresta')
+            aresta.value = arestapi.toFixed(3)
         }
         if(area){
-            area.value = are.toFixed(2)
+            area.value = are.toFixed(3)
             
         }else{
             area = document.getElementById('area')
-            area.value = are.toFixed(2)
+            area.value = are.toFixed(3)
         }
-        if(volume){
-            volume.value = vol.toFixed(2)
+        if(apotemabase){
+            apotemabase.value = apobase.toFixed(3)
         }else{
-            volume = document.getElementById('volume')
-            volume.value = vol.toFixed(2)
+            apotemabase = document.getElementById('apótemabase')
+            apotemabase.value = apobase.toFixed(3)
         }
-        if(geratriz){
-            geratriz.value = ger.toFixed(2)
+        if(flateral){
+            flateral.value = fl.toFixed(3)
         }else{
-            geratriz = document.getElementById('geratriz')
-            geratriz.value = ger.toFixed(2)
+            flateral = document.getElementById('flateral')
+            flateral.value = fl.toFixed(3)
         }
-        var areadabase = pi * (rai**2)
-        var areadalateral = pi * rai * alt
-        var lblareabase = document.createElement('label')
-        lblareabase.id = "lblareabase"
-        lblareabase.classList= "caixa"
-        lblareabase.innerHTML = "Área da base = " + areadabase.toFixed(2) + "u<sup>2<sup>" + "<br>"
-        campos.appendChild(lblareabase)
-        var lblarealateral = document.createElement('label')
-        lblarealateral.id = "lblarealateral"
-        lblarealateral.classList= "caixa"
-        lblarealateral.innerHTML = "Área da lateral = " + areadalateral.toFixed(2) + "u<sup>2<sup>"
-        campos.appendChild(lblarealateral)
-
+      
     }
     
 }
