cf6068d85186f0dc4150892e04c9349dd8d6ccfa
Danilo Ferreira <dan.ali.ferreira@hotmail.com>
2022-07-31 17:31:20 -0300

    3107

--- logica resistividade.js
+++ logica resistividade.js
@@ -6,18 +6,15 @@ function calcular(){
         var resistencia = document.getElementById('resistencia')
         var resis = Number(resistencia.value)
     }
-    if(document.getElementById('resistividade')){
-        var resistividade = document.getElementById('resistividade')
-        var ten = Number(resistividade.value)
-    }
     if(document.getElementById('comprimento')){
         var comprimento = document.getElementById('comprimento')
-        var cor = Number(comprimento.value)
+        var com = Number(comprimento.value)
     }
     if(document.getElementById('area')){
         var area = document.getElementById('area')
-        var res = Number(area.value)
+        var are = Number(area.value)
     }
+
     var control = false
     if(document.getElementById('lblerro')){
         document.getElementById('lblerro').remove()
@@ -25,111 +22,129 @@ function calcular(){
     if(document.getElementById('lblparametro')){
         document.getElementById('lblparametro').remove()
     }
+    if(document.getElementById('material')){
+        var material = document.getElementById('material')
+        var mater = 0
+    }
+    if(material.value == "aluminio" ){
+        mater = 0.02857
+    }
+    if(material.value == "antimonia"){
+        mater = 0.417
+    }
+    if(material.value == "bronze"){
+        mater = 0.067
+    }
+    if(material.value == "chumbo"){
+        mater = 0.22
+    }
+    if(material.value == "cobre"){
+        mater = 0.0172
+    }
+    if(material.value == "constantan"){
+        mater = 0.5
+    }
+    if(material.value == "estanho"){
+        mater = 0.115
+    }
+    if(material.value == "grafite"){
+        mater = 13
+    }
+    if(material.value == "ferro"){
+        mater = 0.1
+    }
+    if(material.value == "latao"){
+        mater = 0.067
+    }
+    if(material.value == "mercurio"){
+        mater = 0.96
+    }
+    if(material.value == "nicromo"){
+        mater = 1.1
+    }
+    if(material.value == "niquel"){
+        mater = 0.087
+    }
+    if(material.value == "ouro"){
+        mater = 0.024
+    }
+    if(material.value == "prata"){
+        mater = 0.0158
+    }
+    if(material.value == "tungstenio"){
+         mater = 0.055
+    }
+    if(material.value == "zinco"){
+        mater = 0.056
+    }
 
-    //R = (p * L) //A
+    //R = (p * L) /A
+    //resis = (mater * com)/are
+    //com = mater * resis/are
+    //5  = (5 * 2) / 2 
+    //10 = 5*x
+    //x = 10/5
+    //com = (resis * are)/mater
+    //
     //R resistência
     //p resistividade do condutor em .m.
     //L comprimento em metro
     //area transversal  mm²
     //Material
-
-/*Resistividade (ohms.m.mm2)
-
-Alumínio  0.029
-Antimônio   0.417
-Bronze      0.067
-Chumbo      0.22
-Cobre puro  0.0162
-Constantan  0.5
-Estanho     0.115
-Grafite     13
-Ferro puro  0.096
-Latão       0.067
-Mercúrio    0.96
-Nicromo     1.1
-Níquel      0.087
-Ouro        0.024
-Prata       0.0158
-Tungstêio   0.055
-Zinco       0.056*/
-
-    
-    if(resis<0 || cor<0 || res<0){
-        erro_negativos()  
-    }else{
-        if(resis && ten && !cor && !res && !control){
-            //i = p/v
-            cor = resis / ten
-            res = ten / cor
-            control = true
-            exibir()
-        }
-        else if(resis && cor && !ten && !res && !control){
-            //v = p/i
-            ten = resis / cor
-            res = ten / cor
-            control = true
-            exibir()
-        }
-        else if(resis && res && !cor && !ten && !control){
-            //i = (p/r)**(1/2)
-            cor = (resis/res)**(1/2)
-            ten = res * cor
-            control = true
-            exibir()
-        }
-        else if(ten && cor && !resis && !res && !control){
-            //p = v*i
-            resis = ten * cor
-            res = ten / cor
-            control = true
-            exibir()
-        }
-        else if(ten && res && !cor && !resis && !control){
-            //p = v**2 / r
-            resis = ten**2 / res
-            cor = resis / ten
-            control = true
-            exibir()
-        }
-        else if(cor && res && !resis && !ten && !control){
-            //v = r*i
-            ten = res * cor
-            resis = ten * cor 
-            control = true
-            exibir()
-        }
-        else{
-            erro_parametros_combinaveis()
-        }
-        
+    /*Resistividade (ohms.m.mm2)
+    Alumínio    0.02857
+    Antimônio   0.417
+    Bronze      0.067
+    Chumbo      0.22
+    Cobre puro  0.0172
+    Constantan  0.5
+    Estanho     0.115
+    Grafite     13
+    Ferro puro  0.096
+    Latão       0.067
+    Mercúrio    0.96
+    Nicromo     1.1
+    Níquel      0.087
+    Ouro        0.024
+    Prata       0.0158
+    Tungstêio   0.055
+    Zinco       0.056*/
+    //resis com are
+    if(resis && com && !are && !control){
+        are = (mater*com)/resis
+        control = true
+        exibir()
+    }
+    if(resis && are && !com && !control){
+        com = (resis * are)/mater
+        control = true
+        exibir()
+    }
+    if(com && are && !resis && !control){
+        resis = (mater * com)/are
+        control = true
+        exibir()
     }
 
     function exibir(){
-        if(resistencia || resis){
+        if(resistencia){
             resistencia.value = resis.toFixed(2)
         }
         else{
             resistencia = document.getElementById('resistencia')
             resistencia.value = resis.toFixed(2)
         }
-        if(resistividade){
-            resistividade.value = ten.toFixed(2)
-        }else{
-            resistividade = document.getElementById('resistividade')
-            resistividade.value = ten.toFixed(2) 
-        }
         if(comprimento){
-            comprimento.value = cor.toFixed(2)
+            comprimento.value = com.toFixed(2)
         }else{
             comprimento = document.getElementById('comprimento')
-            comprimento.value = cor.toFixed(2)
+            comprimento.value = com.toFixed(2)
         }
         if(area){
-            area.value = res.toFixed(2) 
+            area.value = are.toFixed(2) 
         }else{
             area = document.getElementById('area')
-            area.value = res.toFixed(2)
+            area.value = are.toFixed(2)
         }
     }
 }
@@ -137,9 +152,6 @@ Zinco       0.056*/
 function limpar(){
     if(resistencia){
         resistencia.value = false
-    }
-    if(resistividade){
-        resistividade.value = false
     }
     if(comprimento){
         comprimento.value = false
